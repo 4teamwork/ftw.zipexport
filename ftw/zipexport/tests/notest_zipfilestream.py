396c6a7b6316ee56b7f005dd59c58364fffbef95
Lukas Knoepfel <lukas.knoepfel@4teamwork.ch>
2013-09-02 15:12:49 +0200

    Added zipping test.

---
+++ ftw/zipexport/tests/notest_zipfilestream.py
@@ -0,0 +1,14 @@
+from ftw.zipexport.zipfilestream import ZipFile
+from StringIO import StringIO
+from tempfile import NamedTemporaryFile
+
+
+with NamedTemporaryFile(prefix="zipfilestream_") as tmp_file:
+    with ZipFile(tmp_file.name, "w") as zip_file:
+        sio = StringIO("test")
+        zip_file.writefile(sio, "test.txt")
+        print "Wrote StringIO into test.txt"
+        f = open("test_zipfilestream.py", "r")
+        zip_file.writefile(f, "file.py")
+        print "Wrote file test_zipfilestream.py into file.py"
+        zip_file.printdir()
