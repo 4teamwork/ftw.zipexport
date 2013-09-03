a4d23e8479e80b5ec9106d1ec8fe1ce8e73d6125
Lukas Knoepfel <shylux@gmail.com>
2013-09-03 11:01:28 +0200

    Modified generation. ZipFile now integrated in ZipGenerator contextmanager.

--- ftw/zipexport/generation.py
+++ ftw/zipexport/generation.py
@@ -9,28 +9,28 @@ class ZipGenerator(object):
     """
 
     def __init__(self):
-        self.file_tuples = []
+        self.empty = True
 
     def __enter__(self):
-        self.tmp_zip_file = NamedTemporaryFile(prefix="plone_zipexport_")
-        self.tmp_zip_file.__enter__()
+        self.tmp_file = NamedTemporaryFile(prefix="plone_zipexport_")
+        self.tmp_file.__enter__()
+        self.zip_file = ZipFile(self.tmp_file.name, "w", allowZip64=True)
+        self.zip_file.__enter__()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.tmp_zip_file.__exit__(exc_type, exc_value, traceback)
+        self.zip_file.__exit__(exc_type, exc_value, traceback)
+        self.tmp_file.__exit__(exc_type, exc_value, traceback)
 
     def add_file(self, file_path, file_pointer):
-        self.file_tuples.append((file_path, file_pointer))
+        self.empty = False
+        self.zip_file.writefile(file_pointer, file_path)
 
     @property
     def is_empty(self):
-        return not self.file_tuples
+        return self.empty
 
     def generate(self):
-        if self.tmp_zip_file is None:
+        if self.tmp_file is None:
             raise("Please use ZipGenerator as a context manager.")
-        with ZipFile(self.tmp_zip_file.name, "w", allowZip64=True) as zip_container:
-            for file_path, file_pointer in self.file_tuples:
-                zip_container.writefile(file_pointer, file_path)
-
-        return self.tmp_zip_file
+        return self.tmp_file
