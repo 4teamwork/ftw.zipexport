7a4184c8de724f56f987841097c69040bf1f5ac7
Lukas Knoepfel <shylux@gmail.com>
2013-10-28 13:34:22 +0100

    Added postfix for files with same filename.

--- ftw/zipexport/generation.py
+++ ftw/zipexport/generation.py
@@ -1,5 +1,7 @@
 from tempfile import NamedTemporaryFile
 from ftw.zipexport.zipfilestream import ZipFile
+import os
+import sys
 
 
 class ZipGenerator(object):
@@ -23,12 +25,29 @@ class ZipGenerator(object):
         self.tmp_file.__exit__(exc_type, exc_value, traceback)
 
     def add_file(self, file_path, file_pointer):
+        # paths in zipfile do not have a / at the root
+        file_path = file_path.strip('/')
+
+        file_path = self.generate_unique_filepath(file_path)
+
         try:
             self.zip_file.writefile(file_pointer, file_path)
         except RuntimeError:
             raise StandardError("ZipFile already generated/closed.\
                 Please add all files before generating.")
         self.empty = False
+
+    def generate_unique_filepath(self, file_path):
+        if file_path not in self.zip_file.namelist():
+            return file_path
+
+        path, name = os.path.split(file_path)
+        name, ext = os.path.splitext(name)
+
+        for i in xrange(2, sys.maxint):
+            new_filename = os.path.join(path, '%s (%d)%s' % (name, i, ext))
+            if new_filename not in self.zip_file.namelist():
+                return new_filename
 
     @property
     def is_empty(self):
